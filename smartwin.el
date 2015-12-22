3130773ba1f0f368849123857352ca3879549114
jerryxgh <gh_xu@qq.com>
2015-12-22 10:43:54 +0800

    Reword comment

--- smartwin.el
+++ smartwin.el
@@ -18,17 +18,21 @@
 
 ;; Smartwin is a window for shell buffers, temp buffers and etc.
 
-;; This minor mode let all all shell like buffers share a window, called smart
-;; window, the smart window is always at the bottom of emacs window. Besides
-;; that, when point move to the smart window, it is enlarged automaticly, when
-;; leave the window, it is shrinked automaticly.
-
+;; This minor mode let shell like buffers share a window, called smart window,
+;; the smart window is always at the bottom of emacs window. Besides that, when
+;; point move into or out the smart window, it will be enlarged or shrinked
+;; automaticly.
+;;
 ;; To use smartwin, place this file to load path and add this to .emacs:
-
+;;
 ;; (require 'smartwin)
 ;; (smartwin-mode 1)
+;;
 ;; or run M-x smartwin-mode to toggle it.
-
+;;
+;; To switch between buffers of smart window, you can bind keys like:
+;;     (define-key smartwin-mode-map (kbd "C-c s") 'smartwin-switch-buffer)
+;;
 ;; Then try run M-x shell or or eshell, if you want to show more buffers in
 ;; smart window, please customize variable: smartwin-buffers.
 
